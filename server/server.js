3f5c9a3517b55404f6be9bfcf6f55ef5232b5e09
mdctenga <mdctenga@gmail.com>
2015-08-08 23:50:19 -1000

    Add styles

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Meteor.startup(function() {
   // Check if notes exist. If not, create initial note.
-  if(Notescollection.find().fetch().length === 0){
+  if(NotesCollection.find().fetch().length === 0){
     NotesCollection.insert({
       title: "First Note",
       message: "Don't forget to delete me meow",
